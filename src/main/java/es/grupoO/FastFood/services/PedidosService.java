beb4c8938b6f22b818301ed76e2f1beb4f9f5651
Dani <44318798+GrageraE@users.noreply.github.com>
2026-05-23 12:35:10 +0200

    Formatear codigo

--- src/main/java/es/grupoO/FastFood/services/PedidosService.java
+++ src/main/java/es/grupoO/FastFood/services/PedidosService.java
@@ -75,12 +75,12 @@ public class PedidosService {
             throw new NoExistDBException("El pedido no esta registrado");
         }
         EstadoPedido estadoPedido = EstadoPedido.fromInteger(estado);
-        if(EstadoPedido.ENTREGADO.equals(estadoPedido)){
-        this.lineaPlatosRepository.deleteAll(pedido.getPlatos());
-        this.pedidosRepository.deleteById(idPedido);
-        }else{
-        pedido.setEstado(estadoPedido);
-        this.pedidosRepository.save(pedido);
+        if(EstadoPedido.ENTREGADO.equals(estadoPedido)) {
+            this.lineaPlatosRepository.deleteAll(pedido.getPlatos());
+            this.pedidosRepository.deleteById(idPedido);
+        } else {
+            pedido.setEstado(estadoPedido);
+            this.pedidosRepository.save(pedido);
         }
     }
     
