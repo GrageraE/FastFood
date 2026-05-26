7f14bf91d514f6a8b38c11301f88798e15db94ac
Dani <44318798+GrageraE@users.noreply.github.com>
2026-05-27 01:55:17 +0200

    Añadir testcontainers como dependencia

--- src/test/java/es/grupoO/FastFood/FastFoodApplicationTests.java
+++ src/test/java/es/grupoO/FastFood/FastFoodApplicationTests.java
@@ -1,13 +1,47 @@
 package es.grupoO.FastFood;
 
+import es.grupoO.FastFood.exceptions.NoMatchingPasswordException;
+import es.grupoO.FastFood.services.ClientesService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Testcontainers
 class FastFoodApplicationTests {
+	@Container
+	@ServiceConnection
+	static MongoDBContainer mongoTestContainer
+			= new MongoDBContainer(DockerImageName.parse("mongo:latest"));
+
+	@Autowired
+	private ClientesService clientesService;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testPrueba() {
+		this.clientesService.insertarCliente(
+				"Cl1",
+				"A",
+				"6625",
+				"cl@cl.com",
+				"1234"
+		);
+
+		this.clientesService.validar("cl@cl.com", "1234");
+
+		assertThrows(NoMatchingPasswordException.class,
+				() -> this.clientesService.validar("cl@cl.com", "5678"));
 	}
 
 }
